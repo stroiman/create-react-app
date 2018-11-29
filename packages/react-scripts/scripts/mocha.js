f42634a7c5403fc2b40e324f9d568b6622e05c63
Peter Strøiman <peter@stroiman.com>
2018-11-29 10:35:36 +0100

    Remove unnecessary code from mocha script
    Mocha will spawn a new process, and the code has been setup to run in
    the child process.

--- packages/react-scripts/scripts/mocha.js
+++ packages/react-scripts/scripts/mocha.js
@@ -4,30 +4,6 @@ process.env.BABEL_ENV = 'test';
 process.env.NODE_ENV = 'test';
 process.env.PUBLIC_URL = '';
 
-require('@babel/register');
-require('ignore-styles');
-
-const JSDOM = require('jsdom').JSDOM;
-const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
-const { window } = jsdom;
-function copyProps(src, target) {
-  const props = Object.getOwnPropertyNames(src)
-    .filter(prop => typeof target[prop] === 'undefined')
-    .reduce((result, prop) => ({
-      ...result,
-      [prop]: Object.getOwnPropertyDescriptor(src, prop),
-    }), {});
-  Object.defineProperties(target, props);
-}
-
-global.fetch = require('node-fetch');
-global.window = window;
-global.document = window.document;
-global.navigator = {
-  userAgent: 'node.js',
-};
-copyProps(window, global);
-
 const path = require('path');
 process.argv.push("--require");
 process.argv.push(path.join(__dirname, "utils/mochaInitialization"));
