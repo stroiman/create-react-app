23c0940512361d8eaf41abb4de5dcd6a0ae6d187
Peter Strøiman <peter@stroiman.com>
2018-11-29 15:36:10 +0100

    Include 'mocha' when finding script index

--- packages/react-scripts/bin/react-scripts.js
+++ packages/react-scripts/bin/react-scripts.js
@@ -19,7 +19,7 @@ const spawn = require('react-dev-utils/crossSpawn');
 const args = process.argv.slice(2);
 
 const scriptIndex = args.findIndex(
-  x => x === 'build' || x === 'eject' || x === 'start' || x === 'test'
+  x => x === 'build' || x === 'eject' || x === 'start' || x === 'test' || x === 'mocha'
 );
 const script = scriptIndex === -1 ? args[0] : args[scriptIndex];
 const nodeArgs = scriptIndex > 0 ? args.slice(0, scriptIndex) : [];
